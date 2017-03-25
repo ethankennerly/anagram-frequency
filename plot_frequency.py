cbe599445264cedbc3731d02ee5c26a5a73bd026
Ethan Kennerly <ethan@lumoslabs.com>
2017-03-24 20:20:09 -0700

    Plotted and described most frequent words.

--- plot_frequency.py
+++ plot_frequency.py
@@ -1,5 +1,5 @@
 """
-Plot first and second column.
+See plot_frequency.md
 """
 
 
@@ -9,8 +9,22 @@ from pandas import read_csv
 def plot(word_text_path):
     frame = read_csv(word_text_path, delim_whitespace=True)
     plot = frame.plot(kind='line', loglog=True)
+    png_path = save(plot, '%s.loglog' % word_text_path)
+    print('Plotted to %r' % png_path)
+    fraction = 1.0 / 2.0 ** 10
+    xmax = int(round(len(frame) * fraction))
+    xlim = (0, xmax)
+    plot = frame.plot(kind='line',
+        xlim = xlim)
+    png_path = save(plot, '%s.xlim_%s' % (
+        word_text_path, xmax))
+    print('Plotted to %r' % png_path)
+    return png_path
+
+
+def save(plot, base_path):
     figure = plot.get_figure()
-    png_path = '%s.png' % word_text_path
+    png_path = '%s.png' % base_path
     figure.savefig(png_path)
     return png_path
 
@@ -18,7 +32,6 @@ def plot(word_text_path):
 def plots(text_paths):
     for text_path in text_paths:
         png_path = plot(text_path)
-        print('Plotted to %r' % png_path)
 
 
 if '__main__' == __name__:
